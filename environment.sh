4b4eb787555da983216bbbe36a405685f3336439
Richard Chapman <richard.chapman@digital.cabinet-office.gov.uk>
2018-03-19 12:27:07 +0000

    Deleted the environment.sh file from the repo as it is likely to be udated by mistake in future.

--- environment.sh
+++
@@ -1,7 +0,0 @@
-export STATSD_PREFIX="stats-prefix"
-export NOTIFICATION_QUEUE_PREFIX='testing'
-export NOTIFY_ENVIRONMENT='development'
-export NOTIFICATION_QUEUE_PREFIX='ken'
-export FLASK_APP=application.py
-export FLASK_DEBUG=1
-export WERKZEUG_DEBUG_PIN=off
