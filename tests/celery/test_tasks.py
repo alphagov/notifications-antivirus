459c290b5a10886ea1a0fa745a3f6dd17d753ae8
Richard Chapman <richard.chapman@digital.cabinet-office.gov.uk>
2018-03-26 09:38:41 +0100

    Removed the filename definition per method and made it file level so that it does not have to be redefined for each method. Also updated its name to be a little more generic.

--- tests/celery/test_tasks.py
+++ tests/celery/test_tasks.py
@@ -1,34 +1,33 @@
 from app.celery.tasks import scan_file
 from app.config import QueueNames
 
+TEST_FILENAME = "EXAMPLE-SCAN-LETTER.pdf"
+
 
 def test_test_scan_no_virus(notify_antivirus, mocker):
-    filename = "RC-TEST-SCAN-LETTER.pdf"
-
     mocker.patch('app.celery.tasks._get_letter_pdf', return_value=b"test")
     mocker.patch('app.celery.tasks.clamav_scan', return_value=True)
     mock_send_task = mocker.patch('app.notify_celery.send_task')
 
-    scan_file(filename)
+    scan_file(TEST_FILENAME)
 
     mock_send_task.assert_called_once_with(
         name='process-virus-scan-passed',
-        kwargs={'filename': filename},
+        kwargs={'filename': TEST_FILENAME},
         queue=QueueNames.LETTERS,
     )
 
 
 def test_test_scan_virus_detected(notify_antivirus, mocker):
-    filename = "RC-TEST-SCAN-LETTER.pdf"
 
     mocker.patch('app.celery.tasks._get_letter_pdf', return_value=b"test")
     mocker.patch('app.celery.tasks.clamav_scan', return_value=False)
     mock_send_task = mocker.patch('app.notify_celery.send_task')
 
-    scan_file(filename)
+    scan_file(TEST_FILENAME)
 
     mock_send_task.assert_called_once_with(
         name='process-virus-scan-failed',
-        kwargs={'filename': filename},
+        kwargs={'filename': TEST_FILENAME},
         queue=QueueNames.LETTERS,
     )
