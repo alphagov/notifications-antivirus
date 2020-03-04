7753955ec8d366fd47e91d8948ec20b06727e5e5
Katie Smith <katie.smith@digital.cabinet-office.gov.uk>
2020-03-04 14:30:28 +0000

    Ignore Celery logs by redirecting to /dev/null
    We want to ignore the logs that come from Celery itself since we
    already have logs that cover the tasks - the Celery logs provide
    duplicate data. This copies the approach we use in our delivery apps
    and redirects the Celery logs (which go to stderr by default) to
    `/dev/null`.

--- scripts/run_celery.sh
+++ scripts/run_celery.sh
@@ -4,4 +4,4 @@ set -e
 
 clamd
 
-celery -A run_celery.notify_celery worker --pidfile="/tmp/celery-celery.pid" --loglevel=INFO --concurrency=10
+celery -A run_celery.notify_celery worker --pidfile="/tmp/celery-celery.pid" --loglevel=INFO --concurrency=10 2> /dev/null
