9ec1a453660bcf7e3e58d3b4fb23d91d1dd3b263
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2021-04-13 12:21:17 +0100

    Move existing NotifyTask into factory function
    This mirrors the code we have in the API app, and should make the
    actual change / diff easier to read.

--- app/celery/celery.py
+++ app/celery/celery.py
@@ -1,30 +1,35 @@
 from celery import Celery, Task
 
 
+def make_task(app):
+    # this task is nested so that it has access to the original flask application - when celery restarts processes,
+    # it doesn't appear to execute the whole import process again, and as such, flask.current_app no longer has
+    # a context, and throws a RuntimeError. So we need to create an app context from scratch each time.
+    class NotifyTask(Task):
+        abstract = True
+
+        def on_failure(self, exc, task_id, args, kwargs, einfo):
+            # ensure task will log exceptions to correct handlers
+            with app.app_context():
+                app.logger.exception('Celery task {} failed'.format(self.name))
+                super().on_failure(exc, task_id, args, kwargs, einfo)
+
+        def __call__(self, *args, **kwargs):
+            # ensure task has flask context to access config, logger, etc
+            with app.app_context():
+                return super().__call__(*args, **kwargs)
+
+    return NotifyTask
+
+
 class NotifyCelery(Celery):
 
     def init_app(self, app):
-        # this task is nested so that it has access to the original flask application - when celery restarts processes,
-        # it doesn't appear to execute the whole import process again, and as such, flask.current_app no longer has
-        # a context, and throws a RuntimeError. So we need to create an app context from scratch each time.
-        class NotifyTask(Task):
-            abstract = True
-
-            def on_failure(self, exc, task_id, args, kwargs, einfo):
-                # ensure task will log exceptions to correct handlers
-                with app.app_context():
-                    app.logger.exception('Celery task {} failed'.format(self.name))
-                    super().on_failure(exc, task_id, args, kwargs, einfo)
-
-            def __call__(self, *args, **kwargs):
-                # ensure task has flask context to access config, logger, etc
-                with app.app_context():
-                    return super().__call__(*args, **kwargs)
 
         super().__init__(
             app.import_name,
             broker=app.config['CELERY']['broker_url'],
-            task_cls=NotifyTask,
+            task_cls=make_task(app),
         )
 
         self.conf.update(app.config['CELERY'])
