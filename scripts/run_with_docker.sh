d0a49c17d6e3767f2649ac2f780c07601df7860d
Samuel Williams <samuelhwilliams@gmail.com>
2023-04-13 14:34:44 +0100

    Remove space breaking local runs

--- scripts/run_with_docker.sh
+++ scripts/run_with_docker.sh
@@ -6,7 +6,7 @@ source environment.sh
 docker run -it --rm \
   -e NOTIFY_ENVIRONMENT=development \
   -e FLASK_APP=application.py \
-  -e FLASK_DEBUG = 1 \
+  -e FLASK_DEBUG=1 \
   -e AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID:-$(aws configure get aws_access_key_id)} \
   -e AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY:-$(aws configure get aws_secret_access_key)} \
   -e NOTIFY_LOG_PATH=/var/log/notify/antivirus \
