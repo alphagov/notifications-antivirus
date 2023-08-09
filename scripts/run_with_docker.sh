96da229ff72664dfbcb53e9da33446cdde0aa602
Samuel Williams <samuelhwilliams@gmail.com>
2023-08-09 15:21:35 +0100

    Setup consistent logging directory

--- scripts/run_with_docker.sh
+++ scripts/run_with_docker.sh
@@ -9,7 +9,7 @@ docker run -it --rm \
   -e FLASK_DEBUG=1 \
   -e AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID:-$(aws configure get aws_access_key_id)} \
   -e AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY:-$(aws configure get aws_secret_access_key)} \
-  -e NOTIFY_LOG_PATH=/var/log/notify/antivirus \
+  -e NOTIFY_LOG_PATH=/home/vcap/logs \
   -e NOTIFICATION_QUEUE_PREFIX=${NOTIFICATION_QUEUE_PREFIX} \
   -e SENTRY_ENABLED=${SENTRY_ENABLED:-0} \
   -e SENTRY_DSN=${SENTRY_DSN:-} \
