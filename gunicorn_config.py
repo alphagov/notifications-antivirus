60c88e89b9fb90e8a95f31946617f2c981b6eb09
Robert Scott <robert.scott@digital.cabinet-office.gov.uk>
2024-03-25 14:42:16 +0000

    gunicorn_config: use set_gunicorn_defaults from utils

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,25 +1,6 @@
-import os
-import sys
-import traceback
+from notifications_utils.gunicorn_defaults import set_gunicorn_defaults
+
+set_gunicorn_defaults(globals())
+
 
 workers = 4
-errorlog = "/home/vcap/logs/gunicorn_error.log"
-bind = "0.0.0.0:{}".format(os.getenv("PORT"))
-
-
-def on_starting(server):
-    server.log.info("Starting notifications-antivirus API")
-
-
-def worker_abort(worker):
-    worker.log.info("worker received ABORT %s", worker.pid)
-    for _threadId, stack in sys._current_frames().items():
-        worker.log.error("aborting worker\n%s", traceback.format_stack(stack))
-
-
-def on_exit(server):
-    server.log.info("Stopping notifications-antivirus API")
-
-
-def worker_int(worker):
-    worker.log.info("worker: received SIGINT %s", worker.pid)
