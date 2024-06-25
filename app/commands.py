aa793aca349e09406be8b3c358f2924b04d51828
Chris Hill-Scott <me@quis.cc>
2024-06-25 17:23:44 +0100

    Revert "Bump utils to 79.0.0"

--- app/commands.py
+++ app/commands.py
@@ -6,7 +6,7 @@ from flask import current_app
 def list_routes():
     """List URLs of all application routes."""
     for rule in sorted(current_app.url_map.iter_rules(), key=lambda r: r.rule):
-        print("{:10} {}".format(", ".join(rule.methods - {"OPTIONS", "HEAD"}), rule.rule))
+        print("{:10} {}".format(", ".join(rule.methods - set(["OPTIONS", "HEAD"])), rule.rule))
 
 
 def setup_commands(application):
