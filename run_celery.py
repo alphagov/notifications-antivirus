9a503111989dac785c59693a9c1b95e16075bad9
James Robinson <james.robinson@digital.cabinet-office.gov.uk>
2026-04-09 11:38:20 +0100

    Add OTel auto-instrumentation when invoked via Celery

--- run_celery.py
+++ run_celery.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python
 
+import os
+
 import notifications_utils.logging.celery as celery_logging
+from celery.signals import worker_process_init
+from notifications_utils.semconv import set_service_instance_id
+from opentelemetry.instrumentation import auto_instrumentation
 
 from app.performance import init_performance_monitoring
 
@@ -15,3 +20,10 @@ from app import notify_celery, create_app  # noqa
 application = Flask("antivirus")
 create_app(application)
 celery_logging.set_up_logging(application.config)
+
+
+@worker_process_init.connect
+def init_worker(**_) -> None:
+    if os.environ.get("OTEL_SERVICE_NAME") is not None:
+        set_service_instance_id()
+        auto_instrumentation.initialize()
