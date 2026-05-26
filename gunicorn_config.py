1601c2d552c1ec703b252b6e224ac54696afd3ca
James Robinson <james.robinson@digital.cabinet-office.gov.uk>
2026-05-26 17:41:44 +0100

    WIP

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,6 +1,15 @@
+import os
 from notifications_utils.gunicorn.defaults import set_gunicorn_defaults
+from notifications_utils.semconv import set_service_instance_id
+from opentelemetry.instrumentation import auto_instrumentation
 
 set_gunicorn_defaults(globals())
 
 
 workers = 4
+
+if os.environ.get("OTEL_SERVICE_NAME") is not None:
+
+    def post_fork(server, worker):
+        set_service_instance_id()
+        auto_instrumentation.initialize()
